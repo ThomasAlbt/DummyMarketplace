1a545fcb7f63b6af71af5239b1f50d58a1dc8e72
ThomasAlbt <thomas.albert.jv@gmail.com>
2025-07-21 22:15:01 +0100

    changed details Header until I figure a way to make the search in details work

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router';
 import { useState, useEffect } from 'react';
 import checkMobile from '../components/CheckMobile';
+import { useParams } from "react-router";
 
 const Header = ({ search, setSearch }) => {
     const isMobile = checkMobile();
+    const { id } = useParams();
 
     const [theme, setTheme] = useState('light');
     const [activeSearch, setActiveSearch] = useState(true);
@@ -32,20 +34,23 @@ const Header = ({ search, setSearch }) => {
     }
 
     return (
-    <header className='section'>
-        <Link to='/'>
-            {isMobile ? <img src="/LogoMobile.svg" alt="logo dummy"/> : <img src="/logo.svg" alt="logo dummy"/>}
-        </Link>
-        <input
-            type="text"
-            placeholder="Recherche avec le nom d'une annonce"
-            value={inputValue}
-            onChange={e => setInputValue(e.target.value)}
-            className={handleMobileSearch()}
-        />
-        <button id='mobileSearch' onClick={() => setActiveSearch(activeSearch ? false : true)}><img src="/search.svg" alt='icone de recherche'/></button>
-        <button onClick={() => setTheme(theme => (theme === 'light' ? 'dark' : 'light'))} className='circle'>{theme === 'light' ? <img src='/dark.svg'></img> : <img src='/light.svg'></img>}</button>
-    </header>
-)};
+        <header className='section'>
+            <Link to='/'>
+                {isMobile ? <img src="/LogoMobile.svg" alt="logo dummy"/> : <img src="/logo.svg" alt="logo dummy"/>}
+            </Link>
+            {id ? null : 
+            <input
+                type="text"
+                placeholder="Recherche avec le nom d'une annonce"
+                value={inputValue}
+                onChange={e => setInputValue(e.target.value)}
+                className={handleMobileSearch()}
+            />
+            }
+            {id ? null: <button id='mobileSearch' onClick={() => setActiveSearch(activeSearch ? false : true)}><img src="/search.svg" alt='icone de recherche'/></button>}
+            {id ? null: <button onClick={() => setTheme(theme => (theme === 'light' ? 'dark' : 'light'))} className='circle'>{theme === 'light' ? <img src='/dark.svg'></img> : <img src='/light.svg'></img>}</button> }
+        </header>
+    )
+};
 
 export default Header;
